87082c2ae01a239fa2dcad6dce2134864bdfe031
zain ali <mzain42401@gmail.com>
2022-11-14 21:20:13 +0500

    complete

---
+++ app.js
@@ -0,0 +1,29 @@
+// var , let , const 
+
+
+// var 
+
+var num1=29;
+var num2=90;
+var result=num1+num2
+console.log(result);
+
+
+
+
+// let   
+
+
+// let w = "John Doe";
+// let w= 0;              // SyntaxError: 'w' has already been declared
+
+
+
+
+
+// const 
+
+const PI = 3.141592653589793;
+PI = 3.14;      // This will give an error
+PI = PI + 10;   // This will also give an error
+
